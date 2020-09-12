a6e18f14089a52cf2fbd544894eb7b768616f7bb
Mary Waithera <njihiamary11@gmail.com>
2020-09-12 17:31:25 +0300

    use state and usetState hook to add new tasks and update the tasks array

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { nanoid } from "nanoid";
+
 import Todo from './components/Todo';
 import Form from './components/Form';
 import FilterButtons from './components/FilterButton';
@@ -7,16 +9,26 @@ import FilterButtons from './components/FilterButton';
 //pascal-case naming convention distinguishes react components from other JSX elements
 function App(props) {
 
-  //iterate through DATA props
-  //use key prop and assign task id as thi prop's value to ensure that each value is unique
-  const taskList = props.tasks.map(task => (<Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />));
+  //define default tasks to be displayed and function to update this list
+  const [tasks, setTasks] = useState(props.tasks);
+
+  //iterate through the tasks state
+  //use key prop and assign task id as this prop's value to ensure that each value is unique
+  const taskList = tasks.map(task => (<Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />));
+
+  //function defines structure of a new task, copies the existing tasks array, appends new tasks to this copy, and passess this new array to setTasks function to update the component's state
+  function addTask(name) {
+    //nanoid library used to ensure that each new task's id is unique
+    const newTask = {id: "todo-" + nanoid(), name: name, completed: false};
+    setTasks([...tasks, newTask]);
+  }
 
   return (
     <div className="container todoapp stack-large shadow shadow-lg">
     <h1 className="text-center mt-5">
         Welcome to Listify, the site that simplifies to do lists
     </h1>
-    <Form />
+    <Form addTask={addTask}/>
     <FilterButtons />
     <h2 id="list-heading" style={{fontFamily: "'Dosis', sans-serif"}}>
       3 tasks remaining
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function form (props)
+export default function Form (props)
 {
+    //state used instead of props because the data to be displayed by the App component will be created and tracked by this child component
+    //useState hook determines the initial value of a component's state. Returns state and function that updates the state. Array desctructuring used to capture state and function in different variables
+    //ps: unlike props, states can be updated
+
+    const [name, setName] = useState('');
+
+    //function captures users' input
+    function handleChange(e) {
+        setName(e.target.value)
+    }
+
+    //function sends entered data to the parent component
+    function handleSubmit(e) {
+        e.preventDefault();
+        props.addTask(name);
+        setName('');
+    }
+
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
             <h2 className="label-wrapper">
                 <label htmlFor="new-todo-input" className="label__lg" style={{fontFamily: "'Dosis', sans-serif"}}>
                 What needs to be done?
@@ -12,10 +30,12 @@ export default function form (props)
             <input
                 type="text"
                 id="new-todo-input"
-                className="input input__lg"
+                className="input input__lg text-muted"
                 name="text"
                 autoComplete="off"
                 autoFocus="autoFocus"
+                value={name}
+                onChange={handleChange}
             />
             <button type="submit" className="btn btn-info btn__lg shadow shadow-md" style={{fontFamily: "'Dosis', sans-serif"}}>
                 Add A New Task
